ca2c2742dc58fafcea62894f903a85c2c25adf03
Anurag chouhan <chouhansanskar41@gmail.com>
2024-07-19 22:41:29 +0530

    page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us!');
+});
+
+function handleCTAClick() {
+    alert('Welcome! Redirecting you to the sign-up page...');
+}
