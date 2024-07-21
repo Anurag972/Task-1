7571d70e004cfb09c88d1f3266cf422df3cc4e2d
Anurag chouhan <chouhansanskar41@gmail.com>
2024-07-21 13:48:05 +0530

    page.js

--- page.js
+++ page.js
@@ -1,8 +1,3 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for contacting us!');
-});
-
-function handleCTAClick() {
-    alert('Welcome! Redirecting you to the sign-up page...');
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
